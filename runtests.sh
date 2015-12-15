b5843139389b14e6a5fafe0b1cc4c4a81defd28b
jon <jon@personal-dev>
2015-12-15 08:46:40 -0500

    Made some updates to Makefile and runtests.sh for easier testing.

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,3 @@
-make lib_test
 echo -e "\n\nThis is the registry test..."
 ./registry_test
 read n
@@ -7,14 +6,18 @@ echo -e "\n\nThis is the stack test..."
 ./stack_test
 read n
 
-echo "Making binary..."
-make bin
-echo -e "\n\nGoing to run the test brainycake scripts..."
-for f in tests/*
-do
-    echo -e "\n\nProcessing $f...`date`"
-    ./bcinterpreter $f
-    echo -e "\nEnd Time: `date`"
-    read n
-done
-make clean_test
+if [ $# -eq 1 ]
+then
+    if [ $1 == "bin" ]
+    then
+        echo "Making binary..."
+        echo -e "\n\nGoing to run the test brainycake scripts..."
+        for f in tests/*
+        do
+            echo -e "\n\nProcessing $f...`date`"
+            ./bcinterpreter $f
+            echo -e "\nEnd Time: `date`"
+            read n
+        done
+    fi
+fi
